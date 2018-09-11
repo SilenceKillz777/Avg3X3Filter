92e932e684a8191f0a4aa2fad0d5826b4cc9dd60
SilenceKillz777 <yu146287@gmail.com>
2018-09-10 23:06:02 -0400

    Final

--- src/Avg3X3Filter.java
+++ src/Avg3X3Filter.java
@@ -1,5 +1,7 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -7,11 +9,13 @@ public class Avg3X3Filter {
 
 	public static void main(String[] argv) throws IOException{
 		
-		//String line;
 		StringTokenizer str = null;
 		int numRows = 0, numCols = 0, minVal = 0, maxVal = 0;
-		//here
-		Scanner inFile = new Scanner(new File("Avg3X3Filter_Data1.txt"));
+		
+		Scanner inFile = new Scanner(new File(argv[0]));
+		PrintWriter histogram = new PrintWriter(new FileWriter(argv[2]));
+		PrintWriter outFile = new PrintWriter(new FileWriter(argv[3]));
+
 		if(inFile.hasNextLine())
 			str = new StringTokenizer(inFile.nextLine());
 			
@@ -19,83 +23,20 @@ public class Avg3X3Filter {
 		numCols = Integer.parseInt(str.nextToken());
 		minVal = Integer.parseInt(str.nextToken());
 		maxVal = Integer.parseInt(str.nextToken());
-		imageProcessing processor = new imageProcessing(numRows,numCols,minVal,maxVal,inFile);
-		int[][] imgInAry = new int[numRows][numCols], mirrorFramedAry = new int[numRows+2][numCols+2];
-		int[] hist = new int[maxVal+1];
 		
-		//processor.printHeader();
+		imageProcessing processor = new imageProcessing(numRows,numCols,minVal,maxVal,inFile,outFile,histogram);
+		int[][] imgInAry = new int[numRows][numCols], mirrorFramedAry = new int[numRows+2][numCols+2], tempAry = new int[numRows+2][numCols+2], imgOutAry = new int [numRows][numCols];
+		int[] hist = new int[maxVal+1];
+		int thr_value = Integer.parseInt(argv[1]);
+		
 		processor.loadImage(imgInAry,mirrorFramedAry);
 		processor.ComputeHistogram(imgInAry, hist, maxVal);
-		processor.printHist(hist);
 		processor.mirrorFramed(numRows, numCols, mirrorFramedAry);
-		//Test 11:33
-	}
-	
-}
-	
-class imageProcessing {
-	int numRows, numCols, minVal, maxVal, newMin, newMax;
-	int[][] imgInAry, imgOutAry, mirrorFramedAry, tempAry;
-	int[] hist, neighborAry;
-	Scanner inFile;
-	
-	imageProcessing(int numRows,int numCols,int minVal,int maxVal, Scanner inFile){
-		this.numRows = numRows;
-		this.numCols = numCols;
-		this.minVal = minVal;
-		this.maxVal = maxVal;
-		imgInAry= new int [numRows][numCols];
-		imgOutAry = new int [numRows][numCols];
-		mirrorFramedAry = new int [numRows+2][numCols+2];
-		tempAry = new int [numRows+2][numCols+2];
-		hist = new int[maxVal+1];
-		neighborAry = new int[9];
-		this.inFile = inFile;
-	}
-	
-	void loadImage(int[][] imgInAry,int[][] mirrorFramedAry) {
+		processor.computeAVG3X3(mirrorFramedAry, tempAry);
+		processor.computeThreshold(mirrorFramedAry, imgOutAry, thr_value);
 		
-        for(int i=0;i<numRows;i++) {
-            for(int j=0;j<numCols;j++) {
-            	imgInAry[i][j] = Integer.parseInt(inFile.next());
-            	mirrorFramedAry[i+1][j+1] = imgInAry[i][j];
-            }       
-        }
-        
-    }
-	
-	void ComputeHistogram(int[][] imgInAry, int[] hist,int maxVal) {
-		System.out.println(numRows + " " + numCols + " " + minVal + " " + maxVal);
-		for(int i=0;i<numRows;i++){
-			for(int j=0;j<numCols;j++) {
-				hist[imgInAry[i][j]] = hist[imgInAry[i][j]]+1;
-			}
-		}
+		processor.printHist(hist);
+		processor.prettyPrint (imgOutAry);
 	}
 	
-	void printHist(int[] hist) {
-		for(int i=0;i<maxVal+1;i++)
-			System.out.println(i + " " + hist[i]);
-	}
-	
-	void mirrorFramed(int numRows, int numCols,int[][] mirrorFramedAry){
-		for(int i=0;i<numRows;i++) {
-			for(int j=0;j<numCols;j++) {
-				mirrorFramedAry[0][j] = mirrorFramedAry[1][j];
-				mirrorFramedAry[numRows+1][j] = mirrorFramedAry[numRows][j];
-				mirrorFramedAry[i][0] = mirrorFramedAry[i][1];
-				mirrorFramedAry[i][numCols+1] = mirrorFramedAry[i][numCols];
-			}
-		}
-		/*
-		for(int i=0;i<numRows+2;i++) {
-			for(int j=0;j<numCols+2;j++) {
-				System.out.print(mirrorFramedAry[i][j]+" ");
-			}
-			System.out.println();
-		}
-		*/
-	}
-
 }
-
---
+++ src/imageProcessing.java
@@ -0,0 +1,109 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class imageProcessing {
+	int numRows, numCols, minVal, maxVal, newMin, newMax;
+	int[][] imgInAry, imgOutAry, mirrorFramedAry, tempAry;
+	int[] hist, neighborAry;
+	Scanner inFile;
+	PrintWriter outFile, histogram;
+	
+	imageProcessing(int numRows,int numCols,int minVal,int maxVal, Scanner inFile, PrintWriter outFile, PrintWriter histogram){
+		this.numRows = numRows;
+		this.numCols = numCols;
+		this.minVal = minVal;
+		this.maxVal = maxVal;
+		newMin = maxVal;
+		newMax = minVal;
+		imgInAry= new int [numRows][numCols];
+		imgOutAry = new int [numRows][numCols];
+		mirrorFramedAry = new int [numRows+2][numCols+2];
+		tempAry = new int [numRows+2][numCols+2];
+		hist = new int[maxVal+1];
+		neighborAry = new int[9];
+		this.inFile = inFile;
+		this.outFile = outFile;
+		this.histogram = histogram;
+	}
+	
+	void loadImage(int[][] imgInAry,int[][] mirrorFramedAry) {
+        for(int i=0;i<numRows;i++) {
+            for(int j=0;j<numCols;j++) {
+            	imgInAry[i][j] = Integer.parseInt(inFile.next());
+            	mirrorFramedAry[i+1][j+1] = imgInAry[i][j];
+            }       
+        }
+    }
+	
+	void ComputeHistogram(int[][] imgInAry, int[] hist,int maxVal) {
+		for(int i=0;i<numRows;i++){
+			for(int j=0;j<numCols;j++) {
+				hist[imgInAry[i][j]]++;
+			}
+		}
+	}
+	
+	void printHist(int[] hist) {
+		histogram.println(numRows + " " + numCols + " " + newMin + " " + newMax);
+		for(int i=0;i<maxVal+1;i++)
+			histogram.println(i + " " + hist[i]);
+		histogram.close();
+	}
+	
+	void mirrorFramed(int numRows, int numCols,int[][] mirrorFramedAry){
+		for(int i=0;i<numRows;i++) {
+			for(int j=0;j<numCols;j++) {
+				mirrorFramedAry[0][j] = mirrorFramedAry[1][j];
+				mirrorFramedAry[numRows+1][j] = mirrorFramedAry[numRows][j];
+				mirrorFramedAry[i][0] = mirrorFramedAry[i][1];
+				mirrorFramedAry[i][numCols+1] = mirrorFramedAry[i][numCols];
+			}
+		}
+	}
+	
+	void computeAVG3X3(int[][] mirrorFramedAry, int[][]tempAry) {
+		for(int i=1;i<numRows+1;i++) {
+			for(int j=1;j<numCols+1;j++) {
+				int sum = 0;
+				neighborAry[0] = mirrorFramedAry[i][j];
+				neighborAry[1] = mirrorFramedAry[i-1][j-1];
+				neighborAry[2] = mirrorFramedAry[i][j-1];
+				neighborAry[3] = mirrorFramedAry[i+1][j-1];
+				neighborAry[4] = mirrorFramedAry[i-1][j];
+				neighborAry[5] = mirrorFramedAry[i+1][j];
+				neighborAry[6] = mirrorFramedAry[i-1][j+1];
+				neighborAry[7] = mirrorFramedAry[i][j+1];
+				neighborAry[8] = mirrorFramedAry[i+1][j+1];
+				for(int k=0;k<9;k++) {
+					sum += neighborAry[k];
+				}
+				tempAry[i][j] = sum/9;
+				if (tempAry[i][j]<newMin)
+					newMin = tempAry[i][j];
+				if (tempAry[i][j]>newMax)
+					newMax = tempAry[i][j];
+			}
+		}
+	}
+	
+	void computeThreshold(int[][] mirrorFramedAry, int[][] imgOutAry, int thr_value) {
+		for(int i=1;i<numRows+1;i++) {
+			for(int j=1;j<numCols+1;j++) {
+				if(mirrorFramedAry[i][j]>=thr_value)
+					imgOutAry[i-1][j-1]=1;
+				else imgOutAry[i-1][j-1] = 0;
+			}
+		}
+	}
+	void prettyPrint(int[][] imgOutAry) {
+		outFile.println(numRows + " " + numCols + " " + newMin + " " + newMax);
+		for(int i=0;i<numRows;i++) {
+			for(int j=0;j<numCols;j++) {
+				if(imgOutAry[i][j] > 0)outFile.print(imgOutAry[i][j]);
+				else outFile.print("  ");
+			}
+			outFile.println();			
+		}
+		outFile.close();
+	}
+}
